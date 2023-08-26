5f2ddc3310d9448fe8d527db3feb3b7436bd70e5
DespairFactor <matthewalex@outlook.com>
2023-08-25 20:00:08 -0400

    Revert "vh: sched: add sched_newidle_balance"
    This reverts commit 25f045740a510b589dbbcc53bdad3de0ad4d1964.
    
    # Conflicts:
    #	drivers/soc/google/vh/kernel/sched/fair.c
    #	drivers/soc/google/vh/kernel/sched/init.c

--- drivers/soc/google/vh/kernel/sched/init.c
+++ drivers/soc/google/vh/kernel/sched/init.c
@@ -75,8 +75,6 @@ extern void vh_sched_setaffinity_mod(void *data, struct task_struct *task,
 extern void vh_try_to_freeze_todo_logging_pixel_mod(void *data, bool *logging_on);
 extern void rvh_cpumask_any_and_distribute(void *data, struct task_struct *p,
 	const struct cpumask *cpu_valid_mask, const struct cpumask *new_mask, int *dest_cpu);
-void sched_newidle_balance_pixel_mod(void *data, struct rq *this_rq, struct rq_flags *rf,
-		int *pulled_task, int *done);
 extern void rvh_can_migrate_task_pixel_mod(void *data, struct task_struct *p, int dst_cpu,
 						int *can_migrate);
 #if IS_ENABLED(CONFIG_USE_VENDOR_GROUP_UTIL)
@@ -271,11 +269,6 @@ static int vh_sched_init(void)
 	if (ret)
 		return ret;
 #endif
-
-	ret = register_trace_android_rvh_sched_newidle_balance(
-		sched_newidle_balance_pixel_mod, NULL);
-	if (ret)
-		return ret;
 
 	ret = register_trace_android_rvh_post_init_entity_util_avg(
 		rvh_post_init_entity_util_avg_pixel_mod, NULL);
